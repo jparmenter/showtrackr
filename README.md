9e8a3c15e48963d563f20e9c3732f0406e9aa163
Jeremy Parmenter <shabado9889@gmail.com>
2014-09-19 17:59:15 -0700

    Added home page for showtrackr

---
+++ README.md
@@ -0,0 +1,10 @@
+# ShowTrackr
+
+### Prequisites
+
+NodeJs needs to be installed on the machine
+Grunt and Bower installed locally
+
+### Install
+
+### Running
---
+++ public/javascripts/services/show.js
@@ -0,0 +1,10 @@
+(function() {
+  function Show($resource) {
+    return $resource('/api/shows/:_id');
+  }
+
+  Show.$inject = ['$resource'];
+  angular
+    .module('showtrackr')
+    .factory('Show', Show);
+})();
